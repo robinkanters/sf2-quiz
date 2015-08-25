512c4e4dda970fd659eaa5374936e92d28fa61db
Robin Kanters <robin@robinkanters.nl>
2015-08-25 20:23:12 +0200

    Added controller for Quiz

---
+++ src/AppBundle/Controller/QuizController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class QuizController extends Controller
+{
+    /**
+     * @Route("/list")
+     * @Template()
+     */
+    public function listAction()
+    {
+        return array(
+            // ...
+        );
+    }
+
+    /**
+     * @Route("/show")
+     * @Template()
+     */
+    public function showAction()
+    {
+        return array(
+            // ...
+        );
+    }
+
+}
---
+++ src/AppBundle/Tests/Controller/QuizControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class QuizControllerTest extends WebTestCase
+{
+    public function testList()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/list');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testShow()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/show');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+}
