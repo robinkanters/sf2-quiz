90e95814829dedb1a5f8a80e7c628e871ab85d63
Robin Kanters <robin@robinkanters.nl>
2015-08-26 15:55:42 +0200

    let's just dump everything

--- src/AppBundle/Tests/Controller/QuizControllerTest.php
+++ src/AppBundle/Tests/Controller/QuizControllerTest.php
@@ -12,12 +12,9 @@ class QuizControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/quiz/list');
 
-        $block = $crawler->filter('div.text-exception > h1');
-        if ($block->count()) {
-            $error = $block->text();
-        }
+        $block = $crawler->text();
 
-        $this->assertEquals("dummy", $error);
+        $this->assertEquals("dummy", $block);
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
